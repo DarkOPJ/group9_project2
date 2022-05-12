6b1b376807de7a9e2fe21d3a767bf83cb5a0092f
Jeff.B <100956199+DarkOPJ@users.noreply.github.com>
2022-05-12 14:45:30 +0000

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\EXCELISOR\Downloads\Compressed\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\EXCELISOR\Desktop\cpenapp\Auth-with-Flutter-and-Firebase-main"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,55 @@
+import '/api/api.dart';
+import '../main.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+// ignore: camel_case_types
+
+class dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final todoP = Provider.of<TodoProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tasks'),
+        backgroundColor:Color.fromARGB(255, 90, 87, 86),
+      ),
+      body: ListView.builder(
+        shrinkWrap: true,
+        itemCount: todoP.todos.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+              trailing: IconButton(
+                  icon: Icon(Icons.delete, color: Colors.blue),
+                  onPressed: () {
+                    todoP.deleteTodo(todoP.todos[index]);
+                  }),
+              leading: Text
+              (
+                "User_id: "+todoP.todos[index].user_id.toString(),
+                style: TextStyle(color:Colors.blue, fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              title: Text(
+                "Task_id: "+todoP.todos[index].id.toString()+"\n"
+                +todoP.todos[index].title,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                "Due_on: "+todoP.todos[index].description+" Status:"+todoP.todos[index].status,
+                style: TextStyle(fontSize: 15, color: Colors.black),
+              ));
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            size: 30,
+          ),
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (ctx) => HomePage()));
+          }),
+    );
+  }
+}
